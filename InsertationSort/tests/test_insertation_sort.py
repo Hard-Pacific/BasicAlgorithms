f7fe60475d1625c6bdea846a11c1387ad12514a2
HardPacific <93593742+Hard-Pacific@users.noreply.github.com>
2024-07-28 23:18:53 +0500

    Update test_insertation_sort.py
    Replacing the unittest library with pytest

--- InsertationSort/tests/test_insertation_sort.py
+++ InsertationSort/tests/test_insertation_sort.py
@@ -1,36 +1,33 @@
-import unittest
+import pytest
 from src.InsertationSort import InsertationSort
 
 
-class TestInsertationSort(unittest.TestCase):
+class TestInsertationSort:
     """
     Набор юнит-тестов для класса сортировки вставками.
     """
+    @pytest.fixture
+    def insertation_class(self):
+        """
+        Фикстура, создающая экземпляр класса InsertationSort для каждого теста.
+        """
+        yield InsertationSort()
 
-    def setUp(self):
-        self.sort_obj = InsertationSort.InsertationSort()
-
-    def test_insertion_sort(self):
+    def test_insertion_sort(self, insertation_class):
         """
         Тестирование сортировки различных списков.
         """
-        self.assertEqual(
-            self.sort_obj.sort([9, 2, 0, -100, 300, 10000, 0, 9]),
-            [-100, 0, 0, 2, 9, 9, 300, 10000],
-        )
-        self.assertEqual(self.sort_obj.sort([-100, 100]), [-100, 100])
-        self.assertEqual(self.sort_obj.sort([1, 0]), [0, 1])
-        self.assertEqual(self.sort_obj.sort([]), [])
-        self.assertEqual(self.sort_obj.sort([1]), [1])
+        assert insertation_class.sort([1, 2, 3]) == [1, 2, 3]
+        assert insertation_class.sort([9, 2, 0, -100, 300, 10000, 0, 9]) == [-100, 0, 0, 2, 9, 9, 300, 10000]
+        assert insertation_class.sort([-100, 100]) == [-100, 100]
+        assert insertation_class.sort([1, 0]) == [0, 1]
+        assert insertation_class.sort([]) == []
+        assert insertation_class.sort([1]) == [1]
 
-    def test_insertion_sort_typeError(self):
+    def test_insertion_sort_typeError(self, insertation_class):
         """
         Проверка возникновения ошибки
         TypeError при неверном типе входных данных.
         """
-        with self.assertRaises(TypeError):
-            self.sort_obj.sort("Hello World")
-
-
-if __name__ == "__main__":
-    unittest.main()
+        with pytest.raises(TypeError):
+            insertation_class.sort("Hello World")
