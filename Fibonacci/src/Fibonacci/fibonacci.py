43c65409f9b6e284297ce3db89b158d74a941406
HardPacific <93593742+Hard-Pacific@users.noreply.github.com>
2025-07-29 20:03:28 +0300

    rework

--- Fibonacci/src/Fibonacci/fibonacci.py
+++ Fibonacci/src/Fibonacci/fibonacci.py
@@ -2,9 +2,10 @@ class Fibonacci():
     def generate(self, n):
         nums = [1, 1]
         c = 0
-        while nums[c] + nums[c+1] < n:
+        while nums[c] + nums[c+1] <= n:
             nums.append(nums[c] + nums[c+1])
             c += 1
         return nums
     
-    
+a = Fibonacci()
+print(a.generate(5))
